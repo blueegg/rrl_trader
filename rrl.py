98fec966c05ed910f7b9d60630d97a28b437088d
kewen <kewen@komoxo.com>
2016-06-15 23:58:51 +0800

    15th, Jun

--- rrl.py
+++ rrl.py
@@ -20,12 +20,37 @@ if __name__ == "__main__":
     close_price = Read_data("Data/USDINR.xls", "30 min")
     data_return = close_price[1:] / close_price[:-1] - 1
 
-    data_ret = data_return * 100
+    data_return = data_return * 100
     data_mean = data_return.mean()
     data_std = data_return.std()
     data_return = (data_return - data_mean) / data_std
 
     print(np.mean(data_return), np.std(data_return))
+
+    m = 10	#No of lookback 
+    w = np.random.rand(m + 1) * 100	# convention [Ft-1, r1,...,rm,1]
+
+    A = 0
+    B = 0
+    old_A = 0
+    old_B = 0
+
+    T = 2000        #length of data_return
+    Ft = 0
+    Ft1 = 0;
+    delta = 0.00	#transaction costs
+    eta = 0.01	    #Time Window
+    mu = 20	        #notional 
+
+    rho = 100	    #learning rate  
+    #rt is the return between t-1 and t. Ft is the action taken at time t
+    #Run Loop - Minin
+    Ft1 = 0	        #initial action is zero
+    RtVector = zeros(m-1,1)	#vector to store Rt
+    DFDwVector = zeros(m-1,m+1) ; % vector to store DFDw;
+    FtVector = zeros(m-1,1) ; % vector to store Ft 
+    StVector = zeros(m-1,1);
+    cumRt = zeros(m-1,1);
 
     fig = plt.subplots(figsize=(10, 6))
     plt.plot(close_price, '-b', label = "close price")
